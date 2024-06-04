ca185c23da0acfcf47c05921c2b54c3edbeccbb7
katarina1412 <k.katarina86@gmail.com>
2024-06-04 21:51:45 +0200

    defined basic classes and attributes

--- Program.cs
+++ Program.cs
@@ -2,9 +2,43 @@
 {
     internal class Program
     {
+
+        class User
+        {
+            private string? name;
+            private string? email;
+            private string? phoneNumber;
+            private string? password;
+        }
+
+        class Service
+        {
+            private string? serviceType;
+            private string? details;
+        }
+
+        class Booking
+        {
+            private User? user;
+            private Service? service;
+            private DateTime bookingDateTime;
+        }
+
+        class SalonSystem
+        {
+            private User? user;
+            //Register
+            //Login
+            //BookService
+            //Viewbookedservice
+            //Cancelbooking
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //TODO welcome message
+            //TODO user registration/login
+            //TODO MENU
         }
     }
 }

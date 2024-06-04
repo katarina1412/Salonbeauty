e1377987ccb57a2a6c71c3e6681deffd900bf8c8
katarina1412 <k.katarina86@gmail.com>
2024-06-04 22:14:30 +0200

    welcome note,meny

--- Program.cs
+++ Program.cs
@@ -35,10 +35,44 @@
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
             //TODO welcome message
+            Console.WriteLine("Welcome to the Beauty Salon!");
             //TODO user registration/login
+            Console.WriteLine("Please register or login.");
+            //NYI register/login
             //TODO MENU
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Book a service");
+                Console.WriteLine("2. View booked services");
+                Console.WriteLine("3. Cancel a booking");
+                Console.WriteLine("4. Exit");
+
+                
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        // TODO Reservation
+                        break;
+                    case "2":
+                        // TODO reservation view
+                        break;
+                    case "3":
+                        // TODO cancellation of reservation
+                        break;
+                    case "4":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+
         }
     }
 }
--- obj/Debug/net8.0/Salonbeauty.AssemblyInfo.cs
+++ obj/Debug/net8.0/Salonbeauty.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Salonbeauty")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca185c23da0acfcf47c05921c2b54c3edbeccbb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Salonbeauty")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Salonbeauty")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
